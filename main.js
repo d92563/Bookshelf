9e8802348b36c1e9dd794d329686afe49b59ffd7
d92563 <dinel_bob@yahoo.com>
2021-01-14 15:43:08 -0800

    Improved HTML and JS

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ let bookshelf = [
     }
 ];
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+    this.title = `Title: ${title}`;
+    this.author = `Author: ${author}`;
+    this.pages = `Pages: ${pages}`;
     this.read = read;
 };
 
@@ -19,5 +19,62 @@ function addBook(title, author, pages, read) {
 
 };
 
-addBook('aaa', 'bbb', 25, true);
+
+
+//Creates books in the browser.
+function render() {
+
+    const books = document.querySelectorAll('.book');
+    
+    for (let i = 0; i < bookshelf.length; i++){
+        createBook(bookshelf[i]);
+    }
+
+}
+
+
+
+//Creates the book DOM elements.
+function createBook(book) {
+
+    const bookshelfDiv = document.querySelector('#bookshelf-container');
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pagesDiv = document.createElement('div');
+    const readButton = document.createElement('button');
+
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', bookshelf.indexOf(book));
+
+    titleDiv.classList.add('title');
+    titleDiv.innerText = book.title;
+    bookDiv.appendChild(titleDiv);
+
+    authorDiv.classList.add('author');
+    authorDiv.innerText = book.author;
+    bookDiv.append(authorDiv);
+
+    pagesDiv.classList.add('pages');
+    pagesDiv.innerText = book.pages;
+    bookDiv.appendChild(pagesDiv);
+
+    readButton.classList.add('read');
+    if (book.read === true) {
+        readButton.innerText = 'Read';
+    } else {
+        readButton.innerText = 'Not Read';
+    }
+    bookDiv.appendChild(readButton);
+
+
+    bookshelfDiv.appendChild(bookDiv);
+
+
+
+}
+
+
+addBook('aaa', 'bbb', 25, false);
 console.log(bookshelf);
+render();
